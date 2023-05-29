7539b5cd226ac1c5000c1e5e212aa1c3c9a84c1e
tomide <tomideadeyanju@gmail.com>
2023-05-29 23:46:34 +0100

    changes made

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,53 +1,46 @@
-import About from './Components/About/About'
-import Navbar from './Components/Navbar/Navbar'
-import { useEffect, useState } from 'react'
+import About from "./Components/About/About";
+import Navbar from "./Components/Navbar/Navbar";
+import { useEffect, useState } from "react";
 import PropagateLoader from "react-spinners/PropagateLoader";
-import Info from './Components/Info/Info';
-import Work from './Components/work/Work';
-import Built from './Components/Built/Built';
-import NoteWorthy from './Components/Built/NoteWorthy';
+import Info from "./Components/Info/Info";
+import Work from "./Components/work/Work";
+import Built from "./Components/Built/Built";
+import NoteWorthy from "./Components/Built/NoteWorthy";
 
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-  setLoading(true)
-  setTimeout(() => {
-    setLoading(false)
-  }, 2000)
-  }, [])
-  
-  return (
-    
-    <div>
-       
-      {
-      loading ? 
-      <div  className='md:w-screen md:h-screen bg-sky-950 font-sans xs:h-screen xs:h-screen flex justify-center items-center '>
-      <PropagateLoader
-      color={'#00e600'}
-      loading={loading}
-      size={30}
-      aria-label="Loading Spinner"
-      data-testid="loader"
-    
-    />
-    </div>
-    :
-    <div className='md:w-full md:h-full bg-sky-950 font-sans xs:w-full xs:h-full'>
-   
- <Navbar /> 
-<About />
-<Info />
-<Work />
-        <Built />
-        <NoteWorthy />
-    </div>
-    }
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
-      
-      </div>
-  )
+  return (
+    <div>
+      {loading ? (
+        <div className="md:w-screen md:h-screen bg-sky-950 font-sans xs:h-screen xs:h-screen flex justify-center items-center ">
+          <PropagateLoader
+            color={"#00e600"}
+            loading={loading}
+            size={30}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="md:w-full md:h-full bg-sky-950 font-sans xs:w-full xs:h-full">
+          <Navbar />
+          <About />
+          <Info />
+          <Work />
+          <Built />
+          <NoteWorthy />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite-react-app/src/Components/Built/Built.jsx
+++ vite-react-app/src/Components/Built/Built.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Book from "../../assets/BookApp.jpg";
 import Transact from "../../assets/TransactApp.jpg";
+import NFT from "../../assets/NFT.jpg";
 import { FaGithub, FaLink } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const Built = () => {
   return (
-    <div name='work' className="mt-[-5px] 2xl:mt-[-50px]  xs:text-xs w-full h-full  p-4 2xl:p-[100px] ">
-      <div > 
+    <div
+      name="work"
+      className="mt-[-5px] 2xl:mt-[-50px]  xs:text-xs w-full h-full  p-4 2xl:p-[100px] "
+    >
+      <div>
         <h1 className="font-medium text-white text-2xl xs:text-sm md:text-xl  lg:text-2xl">
           {" "}
           <span className="text-lime-500 text-2xl xs:text-sm md:text-xl">
@@ -20,13 +24,7 @@ const Built = () => {
 
       <div className="mt-4 md:flex md:justify-center sm:justify-center ">
         <div className="md:flex md:flex-col md:justify-center sm:justify-center  ">
-          <motion.div 
-            initial={{ opacity: 0, scale: 0, x:100 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 4}}
-            whileInView={{opacity: 1, scale: 1, x: 0}}
-            viewport={{once: true}}
-          className="lg:flex lg:flex-row cursor-pointer xs:flex xs:flex-col md:justify-items-stretch xs:hidden sm:hidden md:hidden ">
+          <div className="lg:flex lg:flex-row cursor-pointer xs:flex xs:flex-col md:justify-items-stretch xs:hidden sm:hidden md:hidden ">
             <img
               src={Book}
               className="w-[600px] h-[400px] xs:hidden sm:hidden lg:block 2xl:block xl:block"
@@ -54,15 +52,9 @@ const Built = () => {
                 <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
               </div>
             </div>
-          </motion.div>
+          </div>
 
-          <motion.div 
-          initial={{ opacity: 0, scale: 0, x:100 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 4}}
-          whileInView={{opacity: 1, scale: 1, x: 0}}
-          viewport={{once: true}}
-          className="lg:flex lg:flex-row cursor-pointer xs:hidden sm:hidden md:hidden xs:flex-col-reverse lg:flex-row-reverse lg:mt-9 sm:mt-9 ">
+          <div className="lg:flex lg:flex-row cursor-pointer xs:hidden sm:hidden md:hidden xs:flex-col-reverse lg:flex-row-reverse lg:mt-9 sm:mt-9 ">
             <img
               src={Transact}
               className="w-[600px] h-[400px] xs:hidden sm:hidden lg:block 2xl:block xl:block"
@@ -87,65 +79,82 @@ const Built = () => {
                 <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
               </div>
             </div>
-          </motion.div>
+          </div>
+
+          <div className="lg:flex lg:flex-row cursor-pointer xs:hidden sm:hidden md:hidden xs:flex-col-reverse lg:flex-row-reverse lg:mt-9 sm:mt-9 ">
+            <img
+              src={NFT}
+              className="w-[600px] h-[400px] xs:hidden sm:hidden lg:block 2xl:block xl:block"
+            />
+            <div className="z-10 xs:mt-[30px]">
+              <h1 className="text-white xs:text-xs text-right">Sea place</h1>
+              <div className="w-[500px] lg:w-[300px]  xs:ml-[5px] xs:text-xs xs:backgroundImage-hero-pattern  sm:ml-[5px] md:ml-[-5px] sm:w-full text-right xs:w-full md:w-[600px] mt-4 mx-[-25px] drop-shadow-lg h-[150px] p-4 bg-sky-200 md:mt-[20px]">
+                <p>An NFT landing page</p>
+              </div>
+              <div className="flex justify-between xs:text-xs mt-4 ml-2 xs:ml-0 text-lime-500 md:ml-0 ">
+                <p>React</p>
+                <p>tailwind css </p>
+              </div>
+
+              <div className=" ml-4 text-2xl justify-between flex ">
+                <FaGithub className="text-white hover:text-lime-500 xs:text-xs" />
+                <a href="https://Stylist-page.vercel.app">
+                <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
+                  </a>{" "}
+               
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       {/*for small phone */}
-      
-        <motion.div
-        initial={{ opacity: 0, scale: 0, x:100 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 4}}
-        whileInView={{opacity: 1, scale: 1, x: 0}}
-        viewport={{once: true}}
-        className=" lg:hidden xl:hidden 2xl:hidden cursor-pointer xs:flex xs:flex-col md:justify-items-stretch ">
-            <div className="z-10">
-              <h1 className="text-white xs:text-xs text-right">
-                Book Web Application
-              </h1>
-              <div 
-                style={{ backgroundImage: `url(${Book})`, backgroundSize: 'cover', backgroundColor:'grey', backgroundBlendMode: 'multiply' }}
-              className="w-[500px] lg:w-[300px] sm:ml-[5px] xs:ml-[5px] xs:text-xs md:ml-[-5px] xs:w-full sm:w-full  text-right   mt-4 mx-[-25px] drop-shadow-lg h-[150px] p-4 bg-sky-200 md:mt-[20px]">
-                <p className="text-white">
-                  A web Application that allows users to read books on different
-                  genres, Bookmark books and also create a copy of their own
-                  book
-                </p>
-              </div>
-              <div className="flex justify-between sm:justify-center sm:justify-between xs:text-xs mt-4 ml-2 xs:ml-0 text-lime-500 md:ml-0">
-                <p>React</p>
-                <p>Nodejs </p>
-                <p>MySQL</p>
-                <p>ExpressJs</p>
-              </div>
 
-              <div className="mt-4 ml-4 text-2xl justify-between flex ">
-                <FaGithub className="text-white hover:text-lime-500 xs:text-xs" />
-                <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
-              </div>
-            </div>
-          </motion.div>
+      <div className=" lg:hidden xl:hidden 2xl:hidden cursor-pointer xs:flex xs:flex-col md:justify-items-stretch ">
+        <div className="z-10">
+          <h1 className="text-white xs:text-xs text-right">
+            Book Web Application
+          </h1>
+          <div
+            style={{
+              backgroundImage: `url(${Book})`,
+              backgroundSize: "cover",
+              backgroundColor: "grey",
+              backgroundBlendMode: "multiply",
+            }}
+            className="w-[500px] lg:w-[300px] sm:ml-[5px] xs:ml-[5px] xs:text-xs md:ml-[-5px] xs:w-full sm:w-full  text-right   mt-4 mx-[-25px] drop-shadow-lg h-[150px] p-4 bg-sky-200 md:mt-[20px]"
+          >
+            <p className="text-white">
+              A web Application that allows users to read books on different
+              genres, Bookmark books and also create a copy of their own book
+            </p>
+          </div>
+          <div className="flex justify-between sm:justify-center sm:justify-between xs:text-xs mt-4 ml-2 xs:ml-0 text-lime-500 md:ml-0">
+            <p>React</p>
+            <p>Nodejs </p>
+            <p>MySQL</p>
+            <p>ExpressJs</p>
+          </div>
 
+          <div className="mt-4 ml-4 text-2xl justify-between flex ">
+            <FaGithub className="text-white hover:text-lime-500 xs:text-xs" />
+            <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
+          </div>
+        </div>
+      </div>
 
-
-
-
-
-
-      <motion.div 
-      initial={{ opacity: 0, scale: 0, x:100 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 4}}
-      whileInView={{opacity: 1, scale: 1, x: 0}}
-      viewport={{once: true}}
-      className="lg:flex lg:flex-row cursor-pointer xs:flex xs:flex-col-reverse  lg:hidden xl:hidden 2xl:hidden">
+      <div className="lg:flex lg:flex-row cursor-pointer xs:flex xs:flex-col-reverse  lg:hidden xl:hidden 2xl:hidden">
         <div className="z-10 xs:mt-[30px]">
           <h1 className="text-white xs:text-xs text-right">
             Sales Application
           </h1>
           <div
-            style={{ backgroundImage: `url(${Transact})`, backgroundSize: 'cover', backgroundColor:'grey', backgroundBlendMode: 'multiply' }}
+            style={{
+              backgroundImage: `url(${Transact})`,
+              backgroundSize: "cover",
+              backgroundColor: "grey",
+              backgroundBlendMode: "multiply",
+            }}
             className="w-[500px] lg:w-[300px]  xs:ml-[5px] lg:hidden xl:hidden 2xl:hidden xs:text-xs  sm:ml-[5px]  sm:w-full text-right xs:w-full  mt-4 mx-[-25px] drop-shadow-lg h-[150px] p-4 bg-sky-200 "
           >
             <p className="text-white">
@@ -163,7 +172,42 @@ const Built = () => {
             <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
           </div>
         </div>
-      </motion.div>
+      </div>
+
+
+
+      <div className="lg:flex lg:flex-row cursor-pointer xs:flex xs:flex-col-reverse  lg:hidden xl:hidden 2xl:hidden">
+        <div className="z-10 xs:mt-[30px]">
+          <h1 className="text-white xs:text-xs text-right">
+            Sales Application
+          </h1>
+          <div
+            style={{
+              backgroundImage: `url(${NFT})`,
+              backgroundSize: "cover",
+              backgroundColor: "grey",
+              backgroundBlendMode: "multiply",
+            }}
+            className="w-[500px] lg:w-[300px]  xs:ml-[5px] lg:hidden xl:hidden 2xl:hidden xs:text-xs  sm:ml-[5px]  sm:w-full text-right xs:w-full  mt-4 mx-[-25px] drop-shadow-lg h-[150px] p-4 bg-sky-200 "
+          >
+            <p className="text-white">
+              Sea place
+            </p>
+          </div>
+          <div className="flex justify-between xs:text-xs mt-4 ml-2 xs:ml-0 text-lime-500 md:ml-0 ">
+            <p>React</p>
+            <p>tailwindcss </p>
+          </div>
+
+          <div className=" ml-4 text-2xl justify-between flex ">
+            <FaGithub className="text-white hover:text-lime-500 xs:text-xs" />
+            <a href="https://Stylist-page.vercel.app">
+                <FaLink className="text-white hover:text-lime-500 xs:text-xs" />
+                  </a>{" "}
+               
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
