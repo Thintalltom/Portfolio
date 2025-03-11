5dfbec12f3905125d5c46823a007aa9ee28d2078
Tomide <adeyanjutomide@gmail.com>
2025-03-11 09:09:50 +0100

    Update Built.jsx
    Added changes to the agrisage site and updated it

--- vite-react-app/src/Components/Built/Built.jsx
+++ vite-react-app/src/Components/Built/Built.jsx
@@ -3,7 +3,7 @@ const Built = () => {
   const works = [
     {
       title: 'Agrisage',
-      Link: 'http://afrihack-project.vercel.app/',
+      Link: 'https://agrisagelimited.vercel.app/,
       description: 'This is an AI that predict the harvest time for farmers',
       skills: 'REACTJS + Tailwindcss',
       github: 'https://github.com/Thintalltom/Afrihack-Project'
